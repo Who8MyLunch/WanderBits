92768da01ed7e3fa803377f4466de2da9779aa8b
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-09-22 14:10:13 -0700

    update all the little details for setup.py.

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+from setuptools import setup, find_packages
+
+
+entry_points = {'console_scripts': ['wanderbits = wanderbits:main']}
+
+with open('README.md', 'r') as fi:
+    long_description = fi.read()
+
+# Do it.
+setup(name='WanderBits',
+      description='Wandering Text-Based Adventure Game',
+      long_description=long_description,
+
+      version='0.1.0',
+
+      url='https://github.com/Who8MyLunch/WanderBits',
+
+      author='Pierre V. Villeneuve',
+      author_email='pierre.villeneuve@gmail.com',
+
+      packages=find_packages(),
+      entry_points=entry_points,
+      package_data={'': ['*.txt', '*.md', '*.yml']},
+
+      license='MIT',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: Microsoft',
+                   'Operating System :: POSIX',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Games/Entertainment'],
+
+      zip_safe=False,
+      )
