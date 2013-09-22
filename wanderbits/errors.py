03db562e239c731b6746852757ba95d4ff8eea84
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-09-22 16:14:15 -0700

    Set of game-specific exceptions.

---
+++ wanderbits/errors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+Exceptions for the game WanderBits.
+"""
+
+
+class GameError(Exception):
+    pass
+
+
+class ParserError(GameError):
+    pass
+
+
+class ExecutiveError(GameError):
+    pass
+
+
+class ActionError(GameError):
+    pass
+
+
+class ConsoleError(GameError):
+    pass
+
+
+
