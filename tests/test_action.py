5023cb9bae5098f9019c4e888872ade5e11c89e0
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-09-25 14:35:23 -0700

    empty framework for Action unit tests.

---
+++ tests/test_action.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from __future__ import division, print_function, unicode_literals
+
+
+import os
+import time
+import unittest
+
+from context import wanderbits
+
+
+class Test_Action(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_does_it_init(self):
+        pass
+
+
+
+
+
+# Standalone.
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
