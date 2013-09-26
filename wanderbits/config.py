a1b774a4ed4a7a4c2f8d56b4fb92669a9a9b9d3d
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-09-26 09:22:38 -0700

    initial config file and read/write functions.

---
+++ wanderbits/config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from __future__ import division, print_function, unicode_literals
+
+"""
+Read and write game config files.
+"""
+
+import yaml
+
+def read(fname):
+    """
+    Read game config information from file.
+    """
+    info = yaml.load(open(fname))
+    return info
+
+
+def write(fname, info):
+    """
+    Write game config information to file.
+    """
+    with open(fname, 'w') as fo:
+        yaml.safe_dump(info, stream=fo, width=50,
+                       indent=4, default_flow_style=False)
+
